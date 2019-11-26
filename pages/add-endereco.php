fa30c5dd1e560336dac4cfb454d0100a74e0e9f3
Tecnico em Informatica 2019.1 <16121742019.1@senacrjedu.local>
2019-11-26 18:47:35 -0300

    26112019

--- pages/add-endereco.php
+++ pages/add-endereco.php
@@ -2,31 +2,31 @@
     <form method="POST">
         <div class="form-group">
             <label for="cep">CEP</label>
-            <input type="text" class="form-control" id="cep" name="cep">
+            <input type="text" class="form-control" id="cep" name="cep" onblur="pesquisacep(this.value)" maxlength="9" require>
         </div>
         <div class="form-group">
             <label for="logradouro">Endereço</label>
-            <input type="text" class="form-control" id="logradouro" name="logradouro">
+            <input type="text" class="form-control" id="logradouro" name="logradouro" require>
         </div>
         <div class="form-group">
             <label for="bairro">Bairro</label>
-            <input type="text" class="form-control" id="bairro" name="bairro">
+            <input type="text" class="form-control" id="bairro" name="bairro" require>
         </div>
         <div class="form-group">
             <label for="cidade">Cidade</label>
-            <input type="text" class="form-control" id="cidade" name="cidade">
+            <input type="text" class="form-control" id="cidade" name="cidade" require>
         </div>
 
         <div class="form-group">
             <label for="uf">Estado</label>
-            <input type="text" class="form-control" id="uf" name="uf">
+            <input type="text" class="form-control" id="uf" name="uf" maxlength="2" require>
         </div>
 
         <button type="submit" class="btn btn-primary">Salvar</button>
         <button type="reset" class="btn btn-primary">Cancelar</button>
     </form>
 </div>
-
+<script src="js/viacep.js"></script>
 <?php
 if (!empty($_POST)) {
     require_once("./model/endereco.php");
